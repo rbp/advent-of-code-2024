2dc631cc1cdf392372331b7707ef3d54bf4ed5d3
Rodrigo Pimentel <rbp@isnomore.net>
2024-12-05 10:20:34 +0100

    Day 5, part 2

--- day05/pages.py
+++ day05/pages.py
@@ -16,6 +16,32 @@ def correct_updates(ordering, updates):
             mid_sum += int(update[len(update)//2])
     return mid_sum
 
+def fix_order(ordering, updates):
+    cannot_after = defaultdict(set)
+    for before, after in ordering:
+        cannot_after[after].add(before)
+    
+    mid_sum = 0
+    for update in updates:
+        fixed = False
+        i = 0
+        while i < len(update):
+            page = update[i]
+            j = i+1
+            while j < len(update):
+                if update[j] in cannot_after[page]:
+                    fixed = True
+                    # I could also swap update[i] and update[j]. More CompSci, and would work.
+                    # Also avoids shifing the entire list.
+                    update[i:i] = [update.pop(j)]
+                    break
+                j += 1
+            else:
+                i += 1
+        if fixed:
+            mid_sum += int(update[len(update)//2])
+    return mid_sum
+
 def read_input(f):
     rules = []
     line = f.readline().strip()
@@ -31,6 +57,7 @@ def main():
     with open(infile) as f:
         ordering, updates = read_input(f)
     print(f"Part 1: {correct_updates(ordering, updates)}")
+    print(f"Part 1: {fix_order(ordering, updates)}")
 
 if __name__ == '__main__':
     main()
