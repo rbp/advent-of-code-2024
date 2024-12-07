971a85866ed9e03251fe05268b52dd4e89537465
Rodrigo Pimentel <rbp@isnomore.net>
2024-12-07 13:19:58 +0100

    Day 7, part 2

--- day07/operators.py
+++ day07/operators.py
@@ -1,17 +1,25 @@
 import sys
 
 
-def sum_valid(equations):
-    return sum(result for result, digits in equations if valid(result, digits))
+def sum_valid(equations, concat=False):
+    return sum(
+        result for result, digits in equations if valid(result, digits, concat=concat)
+    )
 
 
-def valid(result, digits, so_far=0):
+def valid(result, digits, concat=False, so_far=0):
     if not digits:
         return so_far == result
 
-    next = digits[0]
-    return valid(result, digits[1:], so_far + next) or valid(
-        result, digits[1:], so_far * next
+    return (
+        valid(result, digits[1:], concat=concat, so_far=so_far + digits[0])
+        or valid(result, digits[1:], concat=concat, so_far=so_far * digits[0])
+        or (
+            concat
+            and valid(
+                result, digits[1:], concat=concat, so_far=int(f"{so_far}{digits[0]}")
+            )
+        )
     )
 
 
@@ -27,6 +35,7 @@ def main():
     with open(infile) as f:
         eqs = read_equations(f)
     print(f"Part 1: {sum_valid(eqs)}")
+    print(f"Part 2: {sum_valid(eqs, concat=True)}")
 
 
 if __name__ == "__main__":
