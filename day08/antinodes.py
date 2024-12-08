2a63f721b256ac9f6a54dbc54f50841f6c9e1772
Rodrigo Pimentel <rbp@isnomore.net>
2024-12-08 10:46:52 +0100

    Day 8, part 2

--- day08/antinodes.py
+++ day08/antinodes.py
@@ -8,26 +8,49 @@ def count_antinodes(map):
     # from the other.
     # antinodes outside the map don't count.
     # Storing the antinode coordinates as (i, j) pairs in a set, you get the total unique count.
+    unique_antinodes = set()
     antennae = defaultdict(list)
     for i in range(len(map)):
         for j in range(len(map[i])):
             if is_antenna(map[i][j]):
                 antennae[map[i][j]].append((i, j))
 
-    unique_antinodes = set()
     for freq in antennae:
         towers = antennae[freq]
         for i in range(len(towers) - 1):
             for j in range(i + 1, len(towers)):
                 a1, a2 = towers[i], towers[j]
                 delta = (a2[0] - a1[0], a2[1] - a1[1])
-                antinodes = (
-                    (a1[0] - delta[0], a1[1] - delta[1]),
-                    (a2[0] + delta[0], a2[1] + delta[1]),
-                )
-                for antinode in antinodes:
+                # Part 1:
+                # antinodes = (
+                #     (a1[0] - delta[0], a1[1] - delta[1]),
+                #     (a2[0] + delta[0], a2[1] + delta[1]),
+                # )
+                # for antinode in antinodes:
+                #     if inside(map, antinode):
+                #         unique_antinodes.add(antinode)
+
+                # Part 2:
+                unique_antinodes.add(a1)
+                unique_antinodes.add(a2)
+                mul = 1
+                while True:
+                    antinode = (a1[0] - mul * delta[0], a1[1] - mul * delta[1])
                     if inside(map, antinode):
                         unique_antinodes.add(antinode)
+                    else:
+                        break
+                    mul += 1
+
+                mul = 1
+                while True:
+                    antinode = (a2[0] + mul * delta[0], a2[1] + mul * delta[1])
+                    if inside(map, antinode):
+                        unique_antinodes.add(antinode)
+                    else:
+                        break
+                    mul += 1
+
     return len(unique_antinodes)
 
 
