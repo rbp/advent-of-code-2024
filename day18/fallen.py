a06297208af706a025852684763f3457033154b1
Rodrigo Pimentel <rbp@isnomore.net>
2025-01-02 14:12:09 +0100

    Day 18, part 2

--- day18/fallen.py
+++ day18/fallen.py
@@ -11,12 +11,73 @@ def steps_after(bytes, n, side):
     map = mkmap(side)
     fall(map, bytes, n)
     show(map)
+    distances = shortest_path(map)
+    return distances
+
+
+def fall_until_blocked(distances, bytes, initial_fall, side):
+    map = mkmap(side)
+    # We know from part 1 that we can fall at least these bytes
+    fall(map, bytes, initial_fall)
+
+    possible_path = path_from_distances(map, distances)
+    for i in range(initial_fall, len(bytes)):
+        fall_byte(map, bytes[i])
+        if can_traverse(map, possible_path):
+            # Actually, we just need to check if the latest fallen byte is in the path...
+            # But fine. This is fast enough.
+            continue
+        distances = shortest_path(map)
+        if distances[0][0] is None:
+            return f"{bytes[i].x},{bytes[i].y}"
+        possible_path = path_from_distances(map, distances)
+        show(map)
     return shortest_path(map)
 
 
 def fall(map, bytes, n):
     for i in range(n):
         map[bytes[i].y][bytes[i].x] = "#"
+
+
+def fall_byte(map, byte):
+    map[byte.y][byte.x] = "#"
+
+
+def path_from_distances(map, distances):
+    start = Point(0, 0)
+    end = Point(len(map) - 1, len(map) - 1)
+    node = start
+    visited = {node}
+    path = [node]
+    while node != end:
+        distance = distances[node.x][node.y]
+        neighbours = [
+            p
+            for p in get_neighbours(map, node, visited)
+            if distances[p.x][p.y] == distance - 1
+        ]
+        node = min(neighbours, key=lambda p: (end.x - p.x) ** 2 + (end.y - p.y) ** 2)
+        visited.add(node)
+        path.append(node)
+    return path
+
+
+def can_traverse(map, path):
+    end = Point(len(map) - 1, len(map) - 1)
+    i = 0
+    while i < len(path):
+        node = path[i]
+        if not valid_pos(map, node):
+            break
+
+        if node == end:
+            # clear()
+            # show(map, path, node)
+            # sleep(0.1)
+            return True
+        i += 1
+    return False
 
 
 def shortest_path(map):
@@ -27,7 +88,6 @@ def shortest_path(map):
     end = Point(len(map) - 1, len(map) - 1)
     distances[end.x][end.y] = 0
     nodes = [(0, end)]
-    iterations = 0
     while nodes:
         distance, node = heapq.heappop(nodes)
         if node in visited:
@@ -37,33 +97,27 @@ def shortest_path(map):
             distances[neighbour.x][neighbour.y] = distance + 1
             heapq.heappush(nodes, (distance + 1, neighbour))
 
-        iterations += 1
-        if iterations % 100 == 0:
-            clear()
-            show(map, visited, node)
-            sleep(0.2)
-
         visited.add(node)
         if start in neighbours:
             break
 
-    return distances[start.x][start.y]
+    return distances
 
 
 def get_neighbours(map, point, visited):
     neighbours = []
     for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         neighbour = Point(point.x + x, point.y + y)
-        if not (
-            0 <= neighbour.x < len(map) and 0 <= neighbour.y < len(map[neighbour.x])
-        ):
-            continue
-        if map[neighbour.x][neighbour.y] == "#":
-            continue
-        if neighbour in visited:
+        if not valid_pos(map, neighbour) or neighbour in visited:
             continue
         neighbours.append(neighbour)
     return neighbours
+
+
+def valid_pos(map, pos):
+    return (0 <= pos.x < len(map) and 0 <= pos.y < len(map[pos.x])) and map[pos.x][
+        pos.y
+    ] != "#"
 
 
 def mkmap(side, ch="."):
@@ -115,7 +169,10 @@ def main():
     with open(infile) as f:
         bytes = read_bytes(f)
 
-    print(f"Part 1: {steps_after(bytes, 1024 if side == 71 else 12, side)}")
+    fallen_bytes = 1024 if side == 71 else 12
+    distances = steps_after(bytes, fallen_bytes, side)
+    print(f"Part 1: {distances[0][0]}")
+    print(f"Part 2: {fall_until_blocked(distances, bytes, fallen_bytes, side)}")
 
 
 if __name__ == "__main__":
